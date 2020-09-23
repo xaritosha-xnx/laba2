2ae2687e674d3ec0d98e98bbf9bc7d6e2ce0023b
xaritosha-xnx <71726367+xaritosha-xnx@users.noreply.github.com>
2020-09-23 12:58:26 +0300

    Add files via upload

---
+++ xnx.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<math.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	printf("\nВведите стороны треугольника\n");
+	float a, b, c;
+	scanf_s("%f%f%f", &a, &b, &c);
+	if (a == b && b == c)
+	{
+		printf("Треугольник равносторонний");
+	}
+	else
+		if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+		{
+			printf("Треугольник прямоугольный");
+		}
+		else
+		{
+			printf("Треугольник простой");
+		}
+	return 0;
+}
